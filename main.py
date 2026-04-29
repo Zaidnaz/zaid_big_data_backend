3b4c86d17f27732e92db3b6866ff59e7de165dd4
Zaidnaz <mahamad9243@gmail.com>
2026-04-29 22:34:00 +0530

    Allow Render origin in CORS

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ origins = ["http://localhost:3000"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_origin_regex=r"https://.*\.(vercel\.app|onrender\.com)",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
